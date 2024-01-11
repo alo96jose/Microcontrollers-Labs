d7b235d6610d70fbf3af94911da7f9d51dbd0caf
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2024-01-10 18:23:24 -0600

    Números aleatorios están funcionando

--- Lab1/src/blink.c
+++ Lab1/src/blink.c
@@ -3,41 +3,51 @@
 #include <stdlib.h>
 
 typedef unsigned int word;
-word __at 0x2007 __CONFIG = (_WDTE_OFF); // Desactiva el Watchdog TImer
+word __at 0x2007 __CONFIG = (_WDTE_OFF); // Desactiva el Watchdog Timer
 
 void delay(unsigned int tiempo);
-void delay_final(void);
 void bcd_convert(int num, int *bcd);
-void processBCD(const int variable1_BCD[4], unsigned char hex_values[4]);
+//int aleatorio();
 
 unsigned int time = 100;
-unsigned int short_time = 30;
+unsigned int lfsr = 0xACE1u;
 
 void main(void)
 {
     TRISIO = 0b00001000; //Poner todos los pines como salidas menos el pin3.
-    GPIO = 0b00001000; //Poner pines en bajo menos el pin3.
+    GPIO = 0x00; //Poner pines en bajo menos el pin3.
 
     unsigned char i=0; // Contador
-    int num_aleatorio=0, variable1=0, variable2=0, resultado=0;
-    int variable1_BCD[4], variable2_BCD[4];
+    unsigned int j=0, num_aleatorio, variable1=0, variable2=0, resultado=0;
+    unsigned int variable_BCD[4];
     unsigned char hex_values[4];
+
+    
 
     // Loop infinito para que bingo no termine.
     while( 1 )
     {
         // While espera que GP3=0. 
         // En este ciclo se produce el número aleatorio
-        for (int j = 0; j < 100; j++)
-        {
-            num_aleatorio=j;
+        j++;
+        if(j==100){
+            num_aleatorio=99;
+            j = 0;
         }
+        else{
+            num_aleatorio = j;
+        }
+        
+        //for (unsigned int j = 0; j < 100; j++)
+        //{
+          //  num_aleatorio=j;
+        //}
 
-        if( GP3 == 0)
+        if(GP3)
         {
-            // Código a ejecutar cuando GP3=0.
-            i++; // Aumento contador en 1.
-            
+            // Código a ejecutar cuando GP3=1.
+            i++; // Aumento contador de pulsos en 1.
+
             if (num_aleatorio < 10)
             {
                 variable1 = 0;
@@ -46,36 +56,42 @@ void main(void)
             else
             {
                 // Extracting the first and second digits
-                int variable1 = num_aleatorio / 10;  // Get the first digit
-                int variable2 = num_aleatorio % 10;  // Get the second digit
+                variable1 = num_aleatorio / 10;  // Get the first digit
+                variable2 = num_aleatorio % 10;  // Get the second digit
             }
-        
+
             // Número de 2 dígitos que esta entre [00,99]
             resultado = (variable1 * 10 + variable2) % 100;
 
             // Guardo número para no repetir (PENDIENTE)
 
-            // Se convierten enteros a código BCD
-            bcd_convert(variable1, variable1_BCD);
-            bcd_convert(variable2, variable2_BCD);
+            // Se convierten enteros variable 1 a código BCD
+            bcd_convert(variable1, variable_BCD);
 
             // Valores en variable1_BCD pasan a hexadecimal
-            processBCD(variable1_BCD, hex_values);
+            for (unsigned char j = 0; j < 4; ++j) {
+            hex_values[j] = (variable_BCD[j] == 0) ? 0x00 : 0xFF;
+            }
 
             // Enciendo display 1 (der)
             GP5 = 0x00;
 
-            // Entrego en pines el código en BCD de variable1´
+            // Entrego en pines el código en BCD de variable1
             GP0 = hex_values[0];
             GP1 = hex_values[1];
             GP2 = hex_values[2];
             GP4 = hex_values[3];
 
+            // Se convierten enteros de variable 2 a código BCD
+            bcd_convert(variable2, variable_BCD);
+
             // Tiempo espera
             delay(time);
 
             // Valores en variable2_BCD pasan a hexadecimal
-            processBCD(variable2_BCD, hex_values);
+            for (unsigned char j = 0; j < 4; ++j) {
+            hex_values[j] = (variable_BCD[j] == 0) ? 0x00 : 0xFF;
+            }
 
             // Enciendo display 2 (izq)
             GP5 = 0xFF;
@@ -89,23 +105,13 @@ void main(void)
             // Tiempo espera
             delay(time);
 
-            // Contar las veces que GP3 se torna 0. Cuando llege a 10 veces, desplegar en los displays 99.
-            if (i==10)
-            {
-                // Función 99 resalta 3 veces
-                delay_final(); // 1
-                delay_final(); // 2
-                delay_final(); // 3
-
-                // Vaciar arreglo con números repetidos
-
-                // Empezar contador
-                i=0;
-            }
+            // GP3 vuelve a ser 0
+            //GP3 = 0x00;
 
         }
         
     }
+
 }
 
 void delay(unsigned int tiempo)
@@ -117,52 +123,6 @@ void delay(unsigned int tiempo)
 	  for(j=0;j<1275;j++);
 }
 
-void delay_final()
-{
-    // Tiempo espera para que aparesca primer 99
-    delay(time);
-
-    // Enciendo display 1 (der)
-    GP5 = 0x00;
-
-    // Poner en pines el número 9. Primer dígito de 99.
-    GP0 = 0x01;
-    GP1 = 0x00;
-    GP2 = 0x00;
-    GP4 = 0x00;
-
-    // Tiempo de espera corto para segundo dígito de 99.
-    // delay(short_time);
-
-    // Enciendo display 2 (izq)
-    GP5 = 0xFF;
-
-    // Poner en pines el número 9. segundo dígito de 99.
-    GP0 = 0x01;
-    GP1 = 0x00;
-    GP2 = 0x00;
-    GP4 = 0x00;
-
-    // Tiempo espera para ver 99 en acción
-    delay(time);
-
-    // Apago display 2
-    GP0 = 0x00;
-    GP1 = 0x00;
-    GP2 = 0x00;
-    GP4 = 0x00;
-
-    // Apago display 1
-    GP5 = 0x00;
-    GP0 = 0x00;
-    GP1 = 0x00;
-    GP2 = 0x00;
-    GP4 = 0x00;
-
-    // Tiempo espera
-    delay(time);
-}
-
 void bcd_convert(int num, int *bcd) {
     bcd[0] = num / 8;
     bcd[1] = (num % 8) / 4;
@@ -170,8 +130,13 @@ void bcd_convert(int num, int *bcd) {
     bcd[3] = num % 2;
 }
 
-void processBCD(const int variable1_BCD[4], unsigned char hex_values[4]) {
-    for (int i = 0; i < 4; ++i) {
-        hex_values[i] = (variable1_BCD[i] == 0) ? 0x00 : 0xFF;
-    }
+/*** 
+int aleatorio(){
+    unsigned bit;
+
+    bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+    lfsr = (lfsr >> 1) | (bit << 15);
+    return lfsr % 100; // Generate a random number between 0 and 99
 }
+
+***/
